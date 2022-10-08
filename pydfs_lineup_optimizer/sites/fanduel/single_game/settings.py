663e5522c88fc444253b02672cb3a6f68a6d434a
eocv2nen <115292159+eocv2nen@users.noreply.github.com>
2022-10-07 22:48:36 -0400

    Update settings.py

--- pydfs_lineup_optimizer/sites/fanduel/single_game/settings.py
+++ pydfs_lineup_optimizer/sites/fanduel/single_game/settings.py
@@ -11,7 +11,7 @@ from pydfs_lineup_optimizer.lineup_exporter import FanDuelCSVLineupExporter
 
 class FanDuelSingleGameSettings(BaseSettings):
     site = Site.FANDUEL_SINGLE_GAME
-    budget = 60000
+    budget = 35000
     max_from_one_team = 4
     csv_importer = FanDuelMVPCSVImporter  # type: Type[FanDuelCSVImporter]
     csv_exporter = FanDuelCSVLineupExporter
